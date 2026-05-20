fe887e7d065d1d7c9ce42e3a1f64b8bb87577fb5
marig012 <mariana.gonzaleza@upb.edu.co>
2026-05-20 00:03:29 -0500

    Remove comment in index.tsx
    Removed comment about redirecting to login.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  // Esto hace que al abrir la app, te lance directamente a tu login
   return <Redirect href="/login" />;
 }
